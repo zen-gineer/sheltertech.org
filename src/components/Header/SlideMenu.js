da18f0a78d66b3dab803b553773f4292eec9db03
Derek Fidler <df@derekfidler.com>
2018-06-04 14:03:51 +0200

    donate button in mobile menu

--- src/components/Header/SlideMenu.js
+++ src/components/Header/SlideMenu.js
@@ -1,6 +1,7 @@
 import { slide as Menu } from 'react-burger-menu'
 import './SlideMenu.scss'
 import React from 'react'
+import ImageDonate from './assets/donate-btn.png'
 
 class SlideMenu extends React.Component {
   showSettings (event) {
@@ -15,6 +16,14 @@ class SlideMenu extends React.Component {
         <a href='/volunteer'>Volunteer</a>
         <a href='/team'>Team</a>
         <a href='mailto:info@sheltertech.org'>Contact Us</a>
+        <a>
+          <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top' id='donate-form'>
+            <input type='hidden' name='cmd' value='_s-xclick' />
+            <input type='hidden' name='hosted_button_id' value='K29R6HRSEDRF6' />
+            <input className='donate-form-button' type='image' src={ImageDonate} border='0' name='submit' alt='PayPal - The safer, easier way to pay online!' />
+            <img alt='' border='0' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1' />
+          </form>
+        </a>
       </Menu>
     )
   }
