808a45349cf8e324745dde732792d3c5cc9a8fb3
Derek Fidler <df@derekfidler.com>
2020-05-26 16:17:27 +0200

    updated get involved page

--- src/routes/GetInvolved/components/PositionsList.js
+++ src/routes/GetInvolved/components/PositionsList.js
@@ -12,40 +12,34 @@ export const PositionsList = () => (
         <p>Description</p>
       </li>
       <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='https://docs.google.com/document/d/10PhfYLb28PqIc2Ef6gwhlStujzP8BPBmvnjcf3W2-3U/edit?usp=sharing' >Chief Financial Officer</a>
-        <a href='https://docs.google.com/document/d/10PhfYLb28PqIc2Ef6gwhlStujzP8BPBmvnjcf3W2-3U/edit?usp=sharing' className='positions-list--item--desc'>Lead ShelterTech's financial strategy and manage our financial operations</a>
+        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Product Management Volunteer' >Product Management</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=Product Management Volunteer' className='positions-list--item--desc'>Lead cross-functional teams of researchers, designers, and developers to improve our products and make lasting impact for our users</a>
       </li>
       <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Growth Volunteer' >Growth</a>
-        <a href='mailto:jasmine@sheltertech.org?subject=Business Development Volunteer' className='positions-list--item--desc'>Help us get our products adopted by service providers and people experiencing homlessness</a>
+        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Growth Volunteer' >Growth & Marketing</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=Growth Volunteer' className='positions-list--item--desc'>Build and execute our GTM strategy for SF Service Guide amongst service providers and low and no-income San Franciscans</a>
       </li>
       <li className='positions-list--item'>
         <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Development Volunteer' >Development</a>
-        <a href='mailto:jasmine@sheltertech.org?subject=Development Volunteer' className='positions-list--item--desc'>Full stack, Front End, and Dev Ops opportunities</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=Development Volunteer' className='positions-list--item--desc'>Full stack, Front End, and Dev Ops opportunities (React or Ruby on Rails preferred)</a>
       </li>
       <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='https://sheltertech.quip.com/cUa8Ay9ui5Ds' >Product</a>
-        <a href='https://sheltertech.quip.com/cUa8Ay9ui5Ds' className='positions-list--item--desc'>Create products for some of the most vulnerable communities</a>
+        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Design Volunteer' >Product Design</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=Design Volunteer' className='positions-list--item--desc'>Work with research, product, and dev teams to create products with real impact</a>
       </li>
       <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Analytics Manager Volunteer' >Analytics Analyst</a>
-        <a href='mailto:jasmine@sheltertech.org?subject=Analytics Manager Volunteer' className='positions-list--item--desc'>Help define and report on organizational metrics as well as assist with city-wide research</a>
+        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=SEO Analyst Volunteer' >SEO Analyst</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=SEO Analyst Volunteer' className='positions-list--item--desc'>Help define and report on organizational metrics as well as assist with city-wide research</a>
       </li>
       <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Legal Volunteer' >Legal</a>
-        <a href='mailto:jasmine@sheltertech.org?subject=Legal Volunteer' className='positions-list--item--desc'>Help us confidently manage our legal requirements</a>
+        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Events Volunteer' >Events</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=Events Volunteer' className='positions-list--item--desc'>Run a variety of events aimed at bringing together the tech and homeless communities</a>
       </li>
       <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='https://sheltertech.quip.com/tXtfAnNkubu1' >Events</a>
-        <a href='https://sheltertech.quip.com/tXtfAnNkubu1' className='positions-list--item--desc'>Run a variety of events aimed at bringing together the tech and homeless communities</a>
-      </li>
-      <li className='positions-list--item'>
-        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=People Ops Volunteer' >People Ops</a>
-        <a href='mailto:jasmine@sheltertech.org?subject=People Ops Volunteer' className='positions-list--item--desc'>Recruit more people to the cause and help manage our growing team</a>
+        <a className='positions-list--item--title' href='mailto:jasmine@sheltertech.org?subject=Volunteer Coordinator' >Volunteer Coordinator</a>
+        <a href='mailto:jasmine@sheltertech.org?subject=Volunteer Coordinator' className='positions-list--item--desc'>Recruit more people to the cause and help manage our growing team</a>
       </li>
     </ul>
-
-    <p className='volunteer--contact'>Don't see your skillset here? Don't worry, there are plenty of other areas where we need help. Just message <a href='mailto:jasmine@sheltertech.org'>Jasmine</a> and ask about volunteering.</p>
   </div>
 )
 
