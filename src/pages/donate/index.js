39569e4870899003e6bd3cd3e95bff55d7469c25
Richard Xia <richardxia@richardxia.com>
2020-09-13 17:17:33 -0700

    Add donate page. (#47)

---
+++ src/pages/donate/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import DonateView from "../../components/pages/donate/DonateView";
+import Layout from "../../layouts/Layout";
+
+export default () => (
+  <Layout>
+    <DonateView />
+  </Layout>
+);
