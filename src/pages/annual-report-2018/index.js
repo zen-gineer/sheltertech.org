1e10fc7fd66d3724304b2bfb971bf6cd5987ca3c
David Agustin <30575095+davidagustin@users.noreply.github.com>
2020-09-13 23:01:07 -0700

    port-impact-page-to-gatsby-1578 (#50)
    Co-authored-by: David Agustin <dagustin@myvest.com>
    Co-authored-by: Richard Xia <richardxia@richardxia.com>

---
+++ src/pages/annual-report-2018/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AnnualReportView from "../../components/pages/annual-report-2018/AnnualReportView";
+import Layout from "../../layouts/Layout";
+
+export default () => (
+  <Layout>
+    <AnnualReportView />
+  </Layout>
+);
