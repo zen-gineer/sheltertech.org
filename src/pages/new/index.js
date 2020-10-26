7251568eb6f19e37473d609232509fb3aebedb07
Candy Wang <39474473+cw323@users.noreply.github.com>
2020-10-25 23:14:22 -0700

    Integrate PartnersAndSponsorsBlock component onto home page (#82)

--- src/pages/new/index.js
+++ src/pages/new/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+import PartnersAndSponsorsBlock from "../../components/grid-aware/PartnersAndSponsorsBlock";
+import benetechLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/benetech-logo.png";
+import ciscoLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/cisco-logo.png";
+import justiceAndDiversityCenterLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/justice-and-diversity-center-logo.png";
+import larkinStreetLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/larkin-street-logo.png";
+import mohcdLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/mohcd-logo.png";
+import pagerdutyLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/pagerduty-logo.png";
+import sfFamiliesLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/sf-families-logo.png";
+import uberLogo from "../../components/grid-aware/PartnersAndSponsorsBlock/stories/uber-logo.png";
 import ThreeParagraphBlock from "../../components/grid-aware/ThreeParagraphBlock";
 import image1 from "../../components/grid-aware/ThreeParagraphBlock/stories/image1.png";
 import image2 from "../../components/grid-aware/ThreeParagraphBlock/stories/image2.png";
@@ -42,6 +51,46 @@ export default () => (
         { text: "Volunteer", internalLink: "/new/volunteer" },
         { text: "Donate", internalLink: "/new/donate" },
         { text: "Work with us", onClick: () => {} },
+      ]}
+    />
+    <PartnersAndSponsorsBlock
+      title="Partners and sponsors"
+      partnersAndSponsors={[
+        {
+          url: mohcdLogo,
+          alt:
+            "Logo of Mayor's Office of Housing and Community Development organization.",
+        },
+        {
+          url: justiceAndDiversityCenterLogo,
+          alt:
+            "Logo of Justice and Diversity Center of The Bar Association of San Francisco organization.",
+        },
+        {
+          url: benetechLogo,
+          alt: "Logo of Benetech nonprofit organization.",
+        },
+        {
+          url: larkinStreetLogo,
+          alt: "Logo of Larkin Street Youth Services nonprofit organization.",
+        },
+        {
+          url: sfFamiliesLogo,
+          alt:
+            "Logo of SF Families online public service directory organization.",
+        },
+        {
+          url: ciscoLogo,
+          alt: "Logo of Cisco Systems networking hardware company.",
+        },
+        {
+          url: pagerdutyLogo,
+          alt: "Logo of PagerDuty computer software company.",
+        },
+        {
+          url: uberLogo,
+          alt: "Logo of Uber Technologies Inc. company.",
+        },
       ]}
     />
   </Layout>
