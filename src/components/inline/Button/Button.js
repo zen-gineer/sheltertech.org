ec0735149737cda8da1d9296a089ec923d69a349
Richard Xia <richardxia@richardxia.com>
2020-11-19 00:57:37 -0800

    Fix button noHover style. (#108)

--- src/components/inline/Button/Button.js
+++ src/components/inline/Button/Button.js
@@ -5,7 +5,7 @@ import React from "react";
 import s from "./Button.module.css";
 
 const Button = ({ text, externalLink, internalLink, onClick, noHover }) => {
-  const button = `${s.button} ${noHover} ? ${s.noHover}: ""}`;
+  const button = `${s.button} ${noHover ? s.noHover : ""}`;
 
   if (externalLink) {
     return (
