ddea8bc49d9cec3b8486fbf7c50bd172885279f6
Richard Xia <richardxia@richardxia.com>
2020-11-24 08:54:31 -0800

    Update Article Spotlight on Volunteer Page with final link. (#135)

--- src/pages/volunteer/index.js
+++ src/pages/volunteer/index.js
@@ -189,7 +189,8 @@ export default () => {
         description="I moved to San Francisco in 2019 and immediately decided to join ShelterTech as I felt compelled to take action to address the worsening homelessness crisis. As Product Lead, I have focused on managing product development and on improving collaboration across all key teams to ensure we build an impactful product."
         button={{
           text: "Read More",
-          externalLink: "/foo",
+          externalLink:
+            "https://medium.com/shelter-tech/volunteer-spotlight-laura-barrera-vera-41594cfbbe7b",
         }}
         backgroundImage={articleSpotlightImage}
       />
