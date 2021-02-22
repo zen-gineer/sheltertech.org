d42e37c4f512b140ba30698285203022a267b124
Candy Wang <39474473+candywang@users.noreply.github.com>
2021-02-21 23:52:59 -0800

    Convert Program card component to TypeScript (#192)
    * Convert to TypeScript
    
    * Add esModuleInterop flag
    
    * Change file extension to tsx
    
    * Revert tsconfig.json

---
+++ src/components/block/ProgramCard/ProgramCard.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import s from "./ProgramCard.module.css";
+
+type ProgramCardProps = {
+  theme: string;
+  image: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+};
+
+const ProgramCard = ({
+  theme,
+  image,
+  imageAlt,
+  title,
+  description,
+}: ProgramCardProps) => (
+  <div
+    className={`${s.programCardContainer} 
+    ${theme === "light" ? s.light : s.dark}`}
+  >
+    <div>
+      <img className={s.img} src={image} alt={imageAlt} />
+    </div>
+    <div className={s.title}>{title}</div>
+    <div className={s.description}>{description}</div>
+  </div>
+);
+
+export default ProgramCard;
