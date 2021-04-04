3cf755d7e289b5c9734e6ac3551eb3958d6f1e65
Ivan Lee <44950287+ivanleeswe@users.noreply.github.com>
2021-04-04 15:28:54 -0700

    Convert BlockQuoteBlock component to TypeScript (#217)
    * Convert BlockQuoteBlock component to TypeScript
    
    * Change height and width of carousel
    
    * Add aspect ratio to height and width of carousel as required

--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.tsx
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.tsx
@@ -1,30 +1,35 @@
-import PropTypes from "prop-types";
 import { CarouselProvider, Slider, Slide, DotGroup } from "pure-react-carousel";
-import React from "react";
+import * as React from "react";
 
 import "pure-react-carousel/dist/react-carousel.es.css";
 import s from "./BlockQuoteBlock.module.css";
 
-const SlideQuote = ({ quote, attribution }) => (
+type SlideQuoteProps = {
+  quote: string;
+  attribution: string;
+};
+
+const SlideQuote = ({ quote, attribution }: SlideQuoteProps) => (
   <figure className={s.slideQuote}>
     <blockquote className={s.quote}>&ldquo;{quote}&rdquo;</blockquote>
     <figcaption className={s.attribution}>&mdash;{attribution}</figcaption>
   </figure>
 );
 
-SlideQuote.propTypes = {
-  quote: PropTypes.string.isRequired,
-  attribution: PropTypes.string.isRequired,
+type BlockQuoteBlockProps = {
+  quotes: SlideQuoteProps[];
 };
 
-const BlockQuoteBlock = ({ quotes }) => (
+const BlockQuoteBlock = ({ quotes }: BlockQuoteBlockProps) => (
   <div className={s.bleedWrapper}>
     <div className={s.carouselWrapper}>
       <CarouselProvider
         totalSlides={quotes.length}
         isIntrinsicHeight
         isPlaying
-        interval="10000"
+        interval={10000}
+        naturalSlideHeight={10}
+        naturalSlideWidth={45}
       >
         <Slider>
           {quotes.map(({ quote, attribution }, index) => (
@@ -38,9 +43,5 @@ const BlockQuoteBlock = ({ quotes }) => (
     </div>
   </div>
 );
-
-BlockQuoteBlock.propTypes = {
-  quotes: PropTypes.arrayOf(PropTypes.shape(SlideQuote.propTypes)).isRequired,
-};
 
 export default BlockQuoteBlock;
