0f40a673a32980b9e65ffd3068d4f6f9c4e43008
David Agustin <30575095+davidagustin@users.noreply.github.com>
2021-04-11 14:57:47 -0700

    Convert OneParagraphBlock component to Typescript (#214)
    * Convert OneParagraphBlock component to Typescript
    
    * Remove comments and change description prop type
    
    Co-authored-by: David Agustin <dagustin@myvest.com>
    Co-authored-by: Ivan Lee <ilee.code@gmail.com>

--- src/components/grid-aware/OneParagraphBlock/OneParagraphBlock.tsx
+++ src/components/grid-aware/OneParagraphBlock/OneParagraphBlock.tsx
@@ -1,11 +1,20 @@
-import PropTypes from "prop-types";
-import React from "react";
+import * as React from "react";
 
-import Button from "../../inline/Button";
+import Button, { ButtonProps } from "../../inline/Button";
 
 import s from "./OneParagraphBlock.module.css";
 
-const OneParagraphBlock = ({ title, descriptions, ctaButtons }) => {
+type OneParagraphBlockProps = {
+  title: string;
+  descriptions: React.ReactNode;
+  ctaButtons: ButtonProps[];
+};
+
+const OneParagraphBlock = ({
+  title,
+  descriptions,
+  ctaButtons,
+}: OneParagraphBlockProps) => {
   const GridAreaLeft = () => (
     <div className={s.gridAreaLeft}>
       <h1 className={s.title}>{title}</h1>
@@ -43,12 +52,6 @@ const OneParagraphBlock = ({ title, descriptions, ctaButtons }) => {
       </div>
     </div>
   );
-};
-
-OneParagraphBlock.propTypes = {
-  title: PropTypes.string.isRequired,
-  descriptions: PropTypes.node.isRequired,
-  ctaButtons: PropTypes.arrayOf(PropTypes.shape(Button.propTypes)).isRequired,
 };
 
 export default OneParagraphBlock;
