630b1df51ce760ca3ad55389ba441703a2ca1815
Ivan Lee <44950287+ivanleeswe@users.noreply.github.com>
2021-05-27 22:15:53 -0700

    Change js files to tsx (#245)
    * Change index.js files to ts and tsx
    
    * Fix lint changes
    
    * Fix lint changes
    
    * Change type of paragraph1 and delete unnecessary string in tsconfig file

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Helmet } from "react-helmet";
 
 import ArticleSpotlightCard from "../../components/grid-aware/ArticleSpotlightCard";
